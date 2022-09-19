9d2ba62c891611aa8762feebd69739d9e69b6d61
Isiah Malit <isiah.malit@publicissapient.com>
2022-09-19 15:43:24 -0400

    removed view model

--- ghibli/ghibli/MovieViewModel/MovieViewModel.swift
+++ ghibli/ghibli/MovieViewModel/MovieViewModel.swift
@@ -1,37 +1,35 @@
+////
+////  MovieViewModel.swift
+////  ghibli
+////
+////  Created by Isiah Marie Ramos Malit on 2022-09-12.
+////
 //
-//  MovieViewModel.swift
-//  ghibli
+//import Foundation
+//import Combine
+//import SwiftUI
 //
-//  Created by Isiah Marie Ramos Malit on 2022-09-12.
+//class MovieViewModel: ObservableObject {
 //
-
-import Foundation
-import Combine
-import SwiftUI
-
-class MovieViewModel: ObservableObject {
-    @Published var toWatch: Bool
-    @Published var watched: Bool
-    
-    var movie: PersonalizedMovie
-
-    init(movie: PersonalizedMovie) {
-        self.movie = movie
-        self.toWatch = movie.state == .toWatch
-        self.watched = movie.state == .watched
-    }
-    
-    func toggle(state: MovieState) {
-        switch state {
-        case .watched:
-            watched = !watched
-            movie.state = watched ? .watched : .none
-        case .toWatch:
-            toWatch = !toWatch
-            movie.state = toWatch ? .toWatch : .none
-        default:
-            break
-        }
-    }
-}
-
+//    var movie: PersonalizedMovie
+//
+//    init(movie: PersonalizedMovie) {
+//        self.movie = movie
+//        self.toWatch = movie.state == .toWatch
+//        self.watched = movie.state == .watched
+//    }
+//
+//    func toggle(state: MovieState) {
+//        switch state {
+//        case .watched:
+//            watched = !watched
+//            movie.state = watched ? .watched : .none
+//        case .toWatch:
+//            toWatch = !toWatch
+//            movie.state = toWatch ? .toWatch : .none
+//        default:
+//            break
+//        }
+//    }
+//}
+//
